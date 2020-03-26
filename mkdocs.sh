9e5ff5a036e63f8d4d875d51b156dce2c30bdecc
knightlf <knight_lf@126.com>
2020-03-26 20:53:57 +0800

    feat： add mkdoc featrue suport to GO1.14
     On branch master
     Your branch is up to date with 'origin/master'.
    
     Changes to be committed:
    	modified:   .idea/workspace.xml
    	deleted:    docs/_config.yml
    	modified:   docs/index.html
    	renamed:    docs/upgrader_lib.html -> docs/lib/index.html
    	modified:   mkdocs.sh

--- mkdocs.sh
+++ mkdocs.sh
@@ -47,4 +47,29 @@ function Set_doc(){
     done
 }
 
-Set_doc
+function Set_doc14(){
+    #dirilist=`find . -type d | while read dir; do echo $dir; done |grep -v \.git |grep -v .idea |grep -v doc`
+    godoc -url /pkg/${gitname} >./docs/index.html
+    for i in  `find . -type d |grep -v \.git |grep -v .idea |grep -v doc |sed 's/..//' |awk 'NR!=1'`
+    do
+    mkdir -p docs/${i}
+    godoc -url /pkg/${gitname}/${i} >./docs/${i}/index.html
+    linkcss='<link type="text/css" rel="stylesheet" href="/'${gitname}'/style.css">'
+    echo $linkcss >>./docs/$i/index.html
+    #if [ -d $basepath"/"${i} ]; then
+    #    read_dir $basepath"/"${i}
+    #    mkdir ./docs/${basepath}
+    #    godoc -all  -html ./ >./docs/index.html
+    #fi
+    done
+}
+
+VER=`go version |awk -F "." '{print $2}'`
+if [ $VER == "11" ]; then
+    echo "This go version is 1.11"
+    Set_doc
+else
+    echo "This go version is 1.14"
+    Set_doc14
+fi
+#Set_doc
